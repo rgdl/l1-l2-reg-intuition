df36501ad1d9ff9437d65f2a45500a9bc87d4e04
rgdl <robert_delisle44@hotmail.com>
2022-06-02 10:44:39 +1000

    conform to QA rules

--- app/app.py
+++ app/app.py
@@ -1,16 +1,16 @@
 from typing import Callable
 
 import numpy as np
-import pandas as pd
-import plotly.express as px
+import pandas as pd  # type: ignore
+import plotly.express as px  # type: ignore
 import streamlit as st
 
-L1 = 'L1'
-L2 = 'L2'
+L1 = "L1"
+L2 = "L2"
 
 
 def true_func(x):
-    y = (x ** 2).sum(1) ** 0.5 < 0.8
+    y = (x**2).sum(1) ** 0.5 < 0.8
     return y.astype(int)
 
 
@@ -18,22 +18,25 @@ def true_func(x):
 class DataCreator:
     """
     We want a dataset which is:
-    * Easy to visualise: maybe a binary classifier over 2 useful input features, along with 2 non-diagnostic features which we don't have to plot
-    * Noisy enough that over-fitting is possible (random swaps of the training labels)
+    * Easy to visualise: maybe a binary classifier over 2 useful input
+    features, along with 2 non-diagnostic features which we don't have to plot
+    * Noisy enough that over-fitting is possible (random swaps of the training
+    labels)
 
-    Note that over-fitting requires a level of complexity in the model also. This might be easiest to achieve with a neural network.
-
+    Note that over-fitting requires a level of complexity in the model also.
+    This might be easiest to achieve with a neural network.
     """
+
     IV_RANGE = (-1, 1)
     N_SAMPLES = 100
 
     @classmethod
     def create_data(
-            cls,
-            n_useful_features: int,
-            n_noise_features: int,
-            noise_amount: float,
-            function: Callable[[np.ndarray], np.ndarray] = true_func,
+        cls,
+        n_useful_features: int,
+        n_noise_features: int,
+        noise_amount: float,
+        function: Callable[[np.ndarray], np.ndarray] = true_func,
     ) -> pd.DataFrame:
 
         if not (0 <= noise_amount <= 1):
@@ -61,38 +64,51 @@ class DataCreator:
         df = pd.DataFrame(
             np.concatenate((useful_features, noise_features), axis=1),
             columns=(
-                *(f'x_useful_{i}' for i in range(n_useful_features)),
-                *(f'x_noise_{i}' for i in range(n_noise_features)),
-            )
+                *(f"x_useful_{i}" for i in range(n_useful_features)),
+                *(f"x_noise_{i}" for i in range(n_noise_features)),
+            ),
         ).assign(y=labels)
 
         return df
 
 
+class Model:
+    def fit(self):
+        print("You fit me!")
+
+
 class App:
     @staticmethod
     def main() -> None:
-        st.header('L1 vs. L2 Regularisation Intuition')
+        st.header("L1 vs. L2 Regularisation Intuition")
 
         # Create data
         data = DataCreator.create_data(2, 2, 0, true_func)
         st.write(data)
 
         # Preview data
-        fig = px.scatter(data.assign(y=data['y'].astype(str)), 'x_useful_0', 'x_useful_1', color='y')
+        fig = px.scatter(
+            data.assign(y=data["y"].astype(str)),
+            "x_useful_0",
+            "x_useful_1",
+            color="y",
+        )
         st.plotly_chart(fig)
 
         # Select Regularisation Option
-        st.selectbox('Choose a regularisation type', (L1, L2))
+        st.selectbox("Choose a regularisation type", (L1, L2))
 
         # Select Regularisation Strength and other things as appropriate
-        st.write('TODO: Select regularisation strength, other parameters?')
+        st.write("TODO: Select regularisation strength, other parameters?")
 
+        model = Model()
+        # Action button
+        st.button("Fit model", on_click=model.fit)
         # See resulting predictions and feature weights
-        st.write('TODO: See resulting predictions and feature weights')
+        st.write("TODO: See resulting predictions and feature weights")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Generate data
     # App should show results that were over-fit, as well as L1 vs. L2 regularised results
     # Also show the weights after each of these 3 scenarios. User can choose which regularisation style is used (or no regularisation), as well as the regularisation strength and maybe the l1 ratio for Elastic Net if it's a mixture of the 2 as I suspect. Are these types of regularisation the same thing as weight decay?
--- test/test.py
+++ test/test.py
@@ -1,35 +1,35 @@
-from app.app import DataCreator
+from app.app import DataCreator  # type: ignore
 
 
 def test_create_data_dtypes():
     df = DataCreator().create_data(2, 2, 0.5)
 
     print(df.dtypes)
-    x_dtypes = df[[col for col in df if col.startswith('x_')]].dtypes
+    x_dtypes = df[[col for col in df if col.startswith("x_")]].dtypes
     assert x_dtypes.nunique() == 1
-    assert 'float' in str(x_dtypes.iloc[0])
+    assert "float" in str(x_dtypes.iloc[0])
 
-    y_dtype = df.dtypes['y']
-    assert 'int' in str(y_dtype)
+    y_dtype = df.dtypes["y"]
+    assert "int" in str(y_dtype)
 
 
 def test_create_data_value_ranges():
     df = DataCreator().create_data(2, 2, 0.5)
 
-    x_vals = df[[col for col in df if col.startswith('x_')]].values
+    x_vals = df[[col for col in df if col.startswith("x_")]].values
     # TODO: this should be determined by function arguments, not some constant!
     assert x_vals.min() >= -1
     assert x_vals.max() <= 1
 
-    y_vals = df['y']
+    y_vals = df["y"]
     assert y_vals.isin({0, 1}).all()
 
 
 def test_create_data_roughly_balanced_classes():
     """
-    WARNING: this is a flaky test, because it depends on probabilistic functions
+    WARNING: this is a flaky test, as it depends on probabilistic functions
     However, it should pass most of the time
     """
     df = DataCreator().create_data(2, 2, 0.5)
 
-    assert df['y'].value_counts(normalize=True).max() < 0.6
+    assert df["y"].value_counts(normalize=True).max() < 0.6
